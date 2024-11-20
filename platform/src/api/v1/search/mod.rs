86f3b6a3cf5ab62253384cbf8baebe9e9d4cfb94
ryancho-0 <ryan@biyard.co>
2024-11-20 20:31:12 +0900

    change search api

--- platform/src/api/v1/search/mod.rs
+++ platform/src/api/v1/search/mod.rs
@@ -2,59 +2,21 @@ use dioxus::prelude::{
     server_fn::codec::{GetUrl, Json},
     ServerFnError, *,
 };
-use serde::{Deserialize, Serialize};
 
 use crate::api::common::CommonQueryResponse;
-use crate::models::survey::{Age, Gender, ProofId, RegionCode, SalaryTier};
 
 #[server(endpoint = "/v1/search", input = GetUrl, output = Json)]
-pub async fn search(q: String) -> Result<CommonQueryResponse<SearchResult>, ServerFnError> {
+pub async fn search(
+    q: String,
+) -> Result<CommonQueryResponse<models::prelude::SearchResult>, ServerFnError> {
     dioxus_logger::tracing::debug!("/v1/search: {:?}", q);
+    let url = if let Some(url) = option_env!("API_URL") {
+        format!("{}/v1/search?query={}", url, q)
+    } else {
+        return Err(ServerFnError::new("\"API URL\" Not found"));
+    };
+    let res = reqwest::get(url).await?;
+    let json_res: CommonQueryResponse<models::prelude::SearchResult> = res.json().await?;
 
-    Ok(CommonQueryResponse {
-        items: vec![SearchResult {
-            proof_id: "proof-id".to_string(),
-            salary_tier: Some(1),
-            region_code: Some(2),
-            age: Some(Age::Specific(20)),
-            gender: None,
-        }],
-        bookmark: None,
-    })
-}
-
-#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "snake_case")]
-#[serde(untagged)]
-pub enum SearchQuery {
-    SearchPanel {
-        // FIXME: it can be modified, because it is a part of noncelab.
-        // e.g. 1, 2, 3, 4, 5
-        salary_tier: Option<SalaryTier>,
-
-        // e.g. 02(Seoul), 051(Busan) and so on.
-        region_code: Option<RegionCode>,
-        gender: Option<Gender>,
-        age: Option<Age>,
-    },
-    SearchAttiributeCertificate {
-        // FIXME: it can be modified, because it is a part of noncelab.
-        // e.g. 1, 2, 3, 4, 5
-        salary_tier: Option<SalaryTier>,
-
-        // e.g. 02(Seoul), 051(Busan) and so on.
-        region_code: Option<RegionCode>,
-        gender: Option<Gender>,
-        age: Option<Age>,
-    },
-}
-
-#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
-pub struct SearchResult {
-    // FIXME: it can be modified, because it is a part of noncelab.
-    proof_id: ProofId,
-    salary_tier: Option<SalaryTier>,
-    region_code: Option<RegionCode>,
-    gender: Option<Gender>,
-    age: Option<Age>,
+    Ok(json_res)
 }
