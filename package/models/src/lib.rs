44da771cef50006e2f81e803f80c35e5622acf3f
ryancho-0 <ryan@biyard.co>
2024-12-02 20:07:58 +0900

    add user model

--- package/models/src/lib.rs
+++ package/models/src/lib.rs
@@ -1,7 +1,10 @@
 mod search;
 mod survey;
+mod user;
 
+pub use crate::user::{AuthDocument, User};
 pub mod prelude {
     pub use crate::search::*;
     pub use crate::survey::*;
+    pub use crate::user::*;
 }
