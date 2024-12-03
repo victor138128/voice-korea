ffc320651732af9e39d13aa4f2a632970cb9c60a
ryancho-0 <ryan@biyard.co>
2024-12-03 10:00:20 +0900

    feat(api): change timestamp type to i64

--- package/models/src/user/mod.rs
+++ package/models/src/user/mod.rs
@@ -9,10 +9,11 @@ pub struct User {
     pub email: String,
     pub password: String,
     pub created_at: i64,
-    pub updated_at: Option<u64>,
-    pub deleted_at: Option<u64>,
+    pub updated_at: Option<i64>,
+    pub deleted_at: Option<i64>,
 }
 
+const VERIFICATION_EXIPIRED_TIME: i64 = 60 * 5;
 impl User {
     pub fn new(email: String, hashed_pw: String) -> Self {
         Self {
@@ -38,7 +39,8 @@ pub struct AuthDocument {
     pub email: String,
     pub value: String,
     pub created_at: i64,
-    pub deleted_at: Option<u64>,
+    pub expired_at: i64,
+    pub attemp_count: u8,
 }
 
 impl AuthDocument {
@@ -49,7 +51,8 @@ impl AuthDocument {
             email,
             value: random_value,
             created_at: Utc::now().timestamp(),
-            deleted_at: None,
+            expired_at: Utc::now().timestamp() + VERIFICATION_EXIPIRED_TIME,
+            attemp_count: 1,
         }
     }
 }
