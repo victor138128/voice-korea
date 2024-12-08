38cd8a293f0efcf8251a71d43d2dc00a1da81568
ryancho-0 <ryan@biyard.co>
2024-12-08 19:38:55 +0900

    feat(platform): update v2/survey api

--- platform/src/api/v2/survey/mod.rs
+++ platform/src/api/v2/survey/mod.rs
@@ -4,27 +4,25 @@ use dioxus::prelude::{
 };
 
 #[server(endpoint = "/v2/surveys", input = GetUrl, output = Json)]
-pub async fn list_surveys() -> Result<Vec<models::prelude::SurveySummary>, ServerFnError> {
+pub async fn list_surveys() -> Result<Vec<models::prelude::ListSurveyResponse>, ServerFnError> {
     use crate::utils::api::ReqwestClient;
 
     let client = ReqwestClient::new()?;
     let res = client.get("/v1/survey").send().await?;
     let res = res.error_for_status()?;
 
-    let mut survey: Vec<models::prelude::SurveySummary> = res.json().await?;
+    let mut survey: Vec<models::prelude::ListSurveyResponse> = res.json().await?;
 
-    let res = client.get("/v1/survey/draft").send().await?;
-    let res = res.error_for_status()?;
+    // let res = client.get("/v1/survey/draft").send().await?;
+    // let res = res.error_for_status()?;
 
-    let survey_draft: models::prelude::ListSurveyDraftResponse = res.json().await?;
-
-    survey.extend(survey_draft.survey);
+    // let survey_draft: models::prelude::ListSurveyDraftResponse = res.json().await?;
 
     Ok(survey)
 }
 
 #[server(endpoint = "/v2/survey", input = GetUrl, output = Json)]
-pub async fn get_survey(id: u32) -> Result<models::prelude::Survey, ServerFnError> {
+pub async fn get_survey(id: String) -> Result<models::prelude::Survey, ServerFnError> {
     use crate::utils::api::ReqwestClient;
 
     let client = ReqwestClient::new()?;
@@ -33,27 +31,15 @@ pub async fn get_survey(id: u32) -> Result<models::prelude::Survey, ServerFnErro
     Ok(res.json().await?)
 }
 
+//change survey status from draft to in_progress
 #[server(endpoint = "/v2/survey/create", input = Json, output = Json)]
 pub async fn create_survey(
-    req: models::prelude::CreateSurveyRequest,
-) -> Result<models::prelude::CreateSurveyResponse, ServerFnError> {
+    id: String,
+) -> Result<models::prelude::ProgressSurveyResponse, ServerFnError> {
     use crate::utils::api::ReqwestClient;
 
     let client = ReqwestClient::new()?;
-    let res = client.post("/v1/survey").json(&req).send().await?;
-    let res = res.error_for_status()?;
-    Ok(res.json().await?)
-}
-
-#[server(endpoint = "/v2/survey/draft", input = GetUrl, output = Json)]
-pub async fn get_survey_draft(id: String) -> Result<models::prelude::Survey, ServerFnError> {
-    use crate::utils::api::ReqwestClient;
-
-    let client = ReqwestClient::new()?;
-    let res = client
-        .get(&format!("/v1/survey/draft/{}", id))
-        .send()
-        .await?;
+    let res = client.post(&format!("/v1/survey/{}", id)).send().await?;
     let res = res.error_for_status()?;
     Ok(res.json().await?)
 }
