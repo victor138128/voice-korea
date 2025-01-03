7f9edb99fa03841c03854731fccd53c80e34c0c5
victor138128 <yepp0517@naver.com>
2025-01-03 09:09:17 +0900

    fix convention

--- platform/src/components/icons/mod.rs
+++ platform/src/components/icons/mod.rs
@@ -110,7 +110,7 @@ pub fn AddUser(width: String, height: String) -> Element {
             width: "24",
             xmlns: "http://www.w3.org/2000/svg",
             height: "24",
-            "viewBox": "0 0 24 24",
+            view_box: "0 0 24 24",
             path {
                 d: "M4 20V19C4 16.2386 6.23858 14 9 14H12.75M17.5355 13.9645V17.5M17.5355 17.5V21.0355M17.5355 17.5H21.0711M17.5355 17.5H14M15 7C15 9.20914 13.2091 11 11 11C8.79086 11 7 9.20914 7 7C7 4.79086 8.79086 3 11 3C13.2091 3 15 4.79086 15 7Z",
                 "stroke-linecap": "round",
@@ -352,7 +352,7 @@ pub fn ModalCancel(width: String, height: String) -> Element {
             fill: "none",
             xmlns: "http://www.w3.org/2000/svg",
             height,
-            "viewBox": "0 0 24 25",
+            view_box: "0 0 24 25",
             path {
                 "stroke-linecap": "round",
                 stroke: "#555462",
